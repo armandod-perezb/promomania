a472d93d9b9143cd7f0cfc93ebf5eee89378f005
armandod-perezb <armandod-perezb@unilibre.edu.co>
2026-03-15 13:46:36 -0500

    rutas mejor definidas

--- lib/Core/app_routes.dart
+++ lib/Core/app_routes.dart
@@ -4,14 +4,37 @@ class AppRoutes {
   static const register = '/register';
   static const forgotPassword = '/forgot_password';
 
+  // Onboarding
+  static const onboarding = '/onboarding';
+  static const onboarding2 = '/onboarding2';
+  static const onboarding3 = '/onboarding3';
+  static const onboarding4 = '/onboarding4';
+
   // Administrator
   static const adminDashboard = '/admin/dashboard';
+  static const manageUsers = '/admin/manage_users';
+  static const managePromotions = '/admin/manage_promotions';
+  static const manageStores = '/admin/manage_stores';
+  static const manageNotifications = '/admin/manage_notifications';
+
   // User
   static const userHome = '/user/home';
-  // Promotions
-  static const promotions = '/promotions';
-  static const promotionDetails = '/promotions/details';
-  // Settings
-  static const settings = '/settings';
+  static const userProfile = '/user/profile';
+  static const userFavorites = '/user/favorites';
+  static const userEdit = '/user/edit';
+  static const explore = '/user/explore';
+  static const promotionDetails = '/user/promotion_details';
 
+  // Promotions
+  static const addPromotions = '/promotions/add';
+  static const addPromotions2 = '/promotions/add2';
+  static const addPromotions3 = '/promotions/add3';
+  static const addPromotions4 = '/promotions/add4';
+  static const addPromotions5 = '/promotions/add5';
+
+  // Settings
+  static const termsService = '/settings/terms_service';
+  static const privacyPolicy = '/settings/privacy_policy';
+  static const aboutUs = '/settings/about_us';
+  static const helpCenter = '/settings/help_center';
 }

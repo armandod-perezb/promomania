2ea83315172254377c89c0cc3d2ec8bb1b918070
armandod-perezb <armandod-perezb@unilibre.edu.co>
2026-03-15 13:53:09 -0500

    rutas en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Core/Routes/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,110 +14,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.onboarding,
+      routes: {
+        /*AppRoutes.login: (context) => const LoginScreen(),
+        AppRoutes.register: (context) => const RegisterScreen(),
+        AppRoutes.forgotPassword: (context) => const ForgotPasswordScreen(),
+        AppRoutes.onboarding: (context) => const OnboardingScreen(),
+        AppRoutes.onboarding2: (context) => const OnboardingScreen2(),
+        AppRoutes.onboarding3: (context) => const OnboardingScreen3(),
+        AppRoutes.onboarding4: (context) => const OnboardingScreen4(),
+        AppRoutes.adminDashboard: (context) => const AdminDashboardScreen(),
+        AppRoutes.manageUsers: (context) => const ManageUsersScreen(),
+        AppRoutes.managePromotions: (context) => const ManagePromotionsScreen(),
+        AppRoutes.manageStores: (context) => const ManageStoresScreen(),
+        AppRoutes.manageNotifications: (context) => const ManageNotificationsScreen(),
+        AppRoutes.userHome: (context) => const UserHomeScreen(),
+        AppRoutes.userProfile: (context) => const UserProfileScreen(),
+        AppRoutes.userFavorites: (context) => const UserFavoritesScreen(),
+        AppRoutes.userEdit: (context) => const UserEditScreen(),
+        AppRoutes.explore: (context) => const ExploreScreen(),
+        AppRoutes.promotionDetails: (context) => const PromotionDetailsScreen(),
+        AppRoutes.addPromotions: (context) => const AddPromotionsScreen(),
+        AppRoutes.addPromotions2: (context) => const AddPromotionsScreen2(),
+        AppRoutes.addPromotions3: (context) => const AddPromotionsScreen3(),
+        AppRoutes.addPromotions4: (context) => const AddPromotionsScreen4(),
+        AppRoutes.addPromotions5: (context) => const AddPromotionsScreen5(),
+        AppRoutes.termsService: (context) => const TermsServiceScreen(),
+        AppRoutes.privacyPolicy: (context) => const PrivacyPolicyScreen(),
+        AppRoutes.aboutUs: (context) => const AboutUsScreen(),
+        AppRoutes.helpCenter: (context) => const HelpCenterScreen(),
+        */
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}

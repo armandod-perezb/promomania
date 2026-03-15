dafd58f6c732daa90dfe9afc520bd9e8de3455f6
armandod-perezb <armandod-perezb@unilibre.edu.co>
2026-03-15 13:19:24 -0500

    archivo de rutas

---
+++ lib/Core/app_routes.dart
@@ -0,0 +1,17 @@
+class AppRoutes {
+  // Authentication
+  static const login = '/login';
+  static const register = '/register';
+  static const forgotPassword = '/forgot_password';
+
+  // Administrator
+  static const adminDashboard = '/admin/dashboard';
+  // User
+  static const userHome = '/user/home';
+  // Promotions
+  static const promotions = '/promotions';
+  static const promotionDetails = '/promotions/details';
+  // Settings
+  static const settings = '/settings';
+
+}

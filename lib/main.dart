4c86cd197015d21d94fe93545615de17b209b8c6
armandod-perezb <armandod-perezb@unilibre.edu.co>
2026-04-28 22:57:30 -0500

    revertir cambios

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       // 👇 CAMBIO PRINCIPAL AQUÍ
-      initialRoute: AppRoutes.onboarding1,
+      initialRoute: AppRoutes.login,
 
       onGenerateRoute: (settings) {
         if (settings.name == AppRoutes.verifyCode) {
